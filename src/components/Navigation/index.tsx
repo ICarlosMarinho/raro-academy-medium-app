8d4bebc014e4e1dc4e960cffb49ffc8303f42dbe
Carlos Marinho <carlos.marinho20@outlook.com>
2022-04-01 00:43:30 -0300

    feat(routing) - adiciona links de autenticação
    Adiciona um _Link_ para a página de login no componente _Navigation,
    para que o usuário possa se autenticar no sistema.
    
    Também foi adicionado uma versão inicial de um _Link_ para que
    o usuário execute o logout no sistema. Esse link só é renderizado
    quando o usuário está logado no sistema.

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
 export const Navigation = () => {
+  const renderAuthLink = () => {
+    return localStorage.getItem("auth_token") ? <Link to="/">Logout</Link> : <Link to="/login">Login</Link>;
+  };
+
   return (
     <>
       <Link to="/">Home</Link>
       <Link to="/artigos">Meus Artigos</Link>
       <Link to="/artigos/novo">Novo Artigo</Link>
+      {renderAuthLink()}
     </>
   );
 };
