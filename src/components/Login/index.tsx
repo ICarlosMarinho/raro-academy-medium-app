5818b09181839ce738ed72fc5c360dae1b49ba17
Carlos Marinho <carlos.marinho20@outlook.com>
2022-04-03 20:35:11 -0300

    feat(integration) - implementa integração em _Login_
    Implementa funções de ação necessárias para que o
    usuário realize autenticação no sistema através da
    página de _login_, utilizando o formulário.

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,46 @@
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { authenticate } from "../../services";
 import { Button } from "../Button";
 import { Input } from "../Input";
+import { setTokenDataToStorage } from "../../helpers";
+import { RequestError } from "./Login.model";
+import { Error } from "../Error";
 
 export const Login = () => {
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [requestError, setRequestError] = useState<RequestError>({ hasError: false, message: "" });
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    setRequestError({ hasError: false, message: "" });
+
+    authenticate(login, senha)
+      .then((tokenData) => {
+        setTokenDataToStorage(tokenData);
+        setLoading(false);
+        navigate("/artigos");
+      })
+      .catch((error) => {
+        setLoading(false);
+        setRequestError({ hasError: true, message: error.message });
+      });
+  };
+
+  const getHandleChange = (setValue: Dispatch<SetStateAction<string>>) => {
+    return (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value);
+    };
+  };
+
+  const renderError = () => {
+    return requestError.hasError ? <Error>{requestError.message}</Error> : null;
+  };
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -12,19 +51,38 @@ export const Login = () => {
             alt="Workflow"
           />
         </div>
-        <form className="mt-8 space-y-6" action="#">
+        <form className="mt-8 space-y-6" action="#" onSubmit={handleSubmit}>
           <div className="rounded-md shadow-sm -space-y-px">
             <div className="mt-5">
-              <Input type="text" id="login" label="Login" placeholder="login" required />
+              <Input
+                type="text"
+                id="login"
+                label="Login"
+                placeholder="login"
+                required
+                value={login}
+                onChange={getHandleChange(setLogin)}
+              />
             </div>
 
             <div className="mt-5">
-              <Input type="password" id="senha" label="senha" placeholder="********" required />
+              <Input
+                type="password"
+                id="senha"
+                label="senha"
+                placeholder="&bull;&bull;&bull;&bull;&bull;&bull;"
+                required
+                value={senha}
+                onChange={getHandleChange(setSenha)}
+              />
             </div>
           </div>
           <div>
-            <Button type="submit">Login</Button>
+            <Button type="submit" disabled={loading}>
+              {loading ? "Carregando..." : "Entrar"}
+            </Button>
           </div>
+          {renderError()}
         </form>
       </div>
     </div>
