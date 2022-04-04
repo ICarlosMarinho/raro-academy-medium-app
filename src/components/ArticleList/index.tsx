a5ece7882aebf2a22d7f40e1b439cea825268b53
Carlos Marinho <carlos.marinho20@outlook.com>
2022-04-03 22:14:08 -0300

    feat(structure) - Adiciona condição de render em _ArticleList_
    Adiciona condição de renderização no componente, de forma que
    ele deverá renderizar uma mensagem informativa quando a lista
    de artigos estiver vazia.

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ArticleThumbnail } from "../ArticleThumbnail";
-import { ArticleListProps } from "./ArticleList.types";
+import { Message } from "../Message";
 
-export const ArticleList: React.FC<ArticleListProps> = ({ articles }) => {
-  return (
+export const ArticleList: React.FC<{ articles: Article[] }> = ({ articles }) => {
+  return articles.length ? (
     <div className="flex flex-col items-center justify-center m-10">
       {articles.map((article) => (
         <ArticleThumbnail key={article.titulo} {...article} />
       ))}
     </div>
+  ) : (
+    <Message variant="info">A lista de artigos está vazia</Message>
   );
 };
